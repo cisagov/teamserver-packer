3e4b02b6bf9a08a3b9bda1a9ac65f28c9e46d0d7
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-01 13:29:24 -0400

    Update terraform code for this project

--- terraform-post-packer/main.tf
+++ terraform-post-packer/main.tf
@@ -43,11 +43,11 @@ locals {
 }
 
 # The most-recent AMI created by cisagov/teamserver-packer
-data "aws_ami" "example" {
+data "aws_ami" "teamserver" {
   filter {
     name = "name"
     values = [
-      "example-hvm-*-x86_64-ebs",
+      "teamserver-hvm-*-x86_64-ebs",
     ]
   }
 
@@ -75,5 +75,5 @@ module "ami_launch_permission" {
   }
 
   account_name_regex = local.account_name_regex
-  ami_id             = data.aws_ami.example.id
+  ami_id             = data.aws_ami.teamserver.id
 }
